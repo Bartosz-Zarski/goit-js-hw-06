0aff66606793ec14d08d754fe316888318c475d2
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-03-24 15:00:47 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# goit-js-hw-06
+# goit-js-hw-06 <br>
+https://bartosz-zarski.github.io/goit-js-hw-06/task-06.html <br>
+https://github.com/Bartosz-Zarski/goit-js-hw-06
--- js/task-06.js
+++ js/task-06.js
@@ -8,18 +8,16 @@ textInput.addEventListener("focus", () => {
   textInput.setAttribute("placeholder", "Please enter 6 symbols");
 });
 
-
-function validate(textInput, dtLength){
-    if (textInput.value.length == dtLength) {
-        textInput.classList.add("valid")
-        textInput.classList.remove("invalid")
-    } else {
-        textInput.classList.add("invalid")
-        textInput.classList.remove("valid")
-    }
-};
+function validate(textInput, dtLength) {
+  if (textInput.value.length == dtLength) {
+    textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
+  } else {
+    textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
+  }
+}
 
 textInput.addEventListener("blur", () => {
-    validate(textInput, dtLength)
-  });
-
+  validate(textInput, dtLength);
+});
