969cac73d1155045d3d361ddc4bf03aee1551d18
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-03-27 01:56:21 +0200

    Mentor fixes

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputEl = document.getElementById("font-size-control");
 const spanEl = document.getElementById("text");
-
+spanEl.style.fontSize = "56px"
 inputEl.addEventListener("input", function () {
   let size = inputEl.value;
   spanEl.style.fontSize = size + "px";
